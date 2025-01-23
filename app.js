95cf5c090d100f6e43fe94da2c0906b75c20c2c3
wendsaw <sawadogo.edgard@gmail.com>
2025-01-23 14:18:05 -0700

    2

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const express =require('express')
+const express =require('express');
 const app=express();
 app.listen(3000);
-app.get('/', (req ,res) =>{
 
-    res.send('<p>home page</>')
+// view engine
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req ,res) =>{
+    res.render('index');
+})
+
+app.get('/about', (req ,res) =>{
+    res.render('about');
+})
+app.use((req,res)=>{
+    res.status(404).render('404');
 })
