526478a8e32f54ae650d38b98d2090860d3e786e
wendsaw <sawadogo.edgard@gmail.com>
2025-01-23 14:49:48 -0700

    3

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ app.listen(3000);
 
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req ,res) =>{
     res.render('index');
 })
-
 app.get('/about', (req ,res) =>{
     res.render('about');
+})
+app.get('/create', (req ,res) =>{
+    res.render('create');
 })
 app.use((req,res)=>{
     res.status(404).render('404');
