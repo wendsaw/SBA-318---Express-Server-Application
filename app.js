f4694926a6ec3117c052bd11d31e099904abea52
wendsaw <sawadogo.edgard@gmail.com>
2025-01-24 21:17:03 -0700

    update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express =require('express');
 const { title } = require('process');
 const morgan=require('morgan')
 const app=express();
+const presidents=require('./models/presidents');
+
 app.listen(3000);
 
 // view engine
@@ -12,14 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(morgan('dev'))
 
-const presidents=[
-    {id:1,lastName:"Washington", firstName:"George", title:"First President"},
-    {id:2,lastName:"Adams", firstName:"John", title:"Second President"},
-    {lid:3,lastName:"Jefferson", firstName:"Thomas", title:"Third President"},
-    {id:4,lastName:"Madison", firstName:"James", title:"Fourth President"},
-    {id:5,lastName:"Trump", firstName:"Donald", title:"Current President"}
-    
-]
+
 
 app.get('/create', (req ,res) =>{
     res.render('create',{title:"New President"});
