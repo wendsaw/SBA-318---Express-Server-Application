f0e97447d6c8d6c01dc7091f734e5bcc349d218d
wendsaw <sawadogo.edgard@gmail.com>
2025-01-25 10:53:42 -0700

    update

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(express.static('public'));
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended:true}));
 
+app.get('/', (req ,res) =>{
+    
+
+    res.render('home',{title:'COMPLETE LIST OF ALL US PRESIDENT'});
+})
+
+
 app.get('/president', (req ,res) =>{
     
 
@@ -24,7 +31,6 @@ app.get('/president', (req ,res) =>{
 app.post('/president',(req,res)=>{
 
     console.log(req.body.firstName);
-    
     
     const presi= {
         id:presidents.length+1,
@@ -41,30 +47,30 @@ app.post('/president',(req,res)=>{
     
 })
 
-app.get('/president/:id',(req,res)=>{
-    const id=req.params.id
-    console.log(id);
-    
-   const presi= presidents.find(c=> c.id===parseInt(req.params.id))
-    
 
-    if (!presidents) res.status(404).send('president not found');
-    res.render('details',{presidents:presi, title:"The president"});
-})
+
+
 
 app.get('/president/create', (req ,res) =>{
+
     res.render('create',{title:"New President"});
 })
 
 
-
-
 app.get('/president/update', (req ,res) =>{
-    res.render('Update',{title:"Update"});
+    res.render('update',{title:"Update"});
 })
 
 app.get('/president/delete', (req ,res) =>{
-    res.render('Delete',{title:"Delete President"});
+    res.render('delete',{title:"Delete President"});
+})
+
+
+app.get('/president/:id',(req,res)=>{
+    const id=req.params.id
+    console.log(id);
+
+    res.render('details')
 })
 
 app.use((req,res)=>{
