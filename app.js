9d8ab1aa02ef85a49cd6ab8b8af7a164a7eeab71
wendsaw <sawadogo.edgard@gmail.com>
2025-01-25 01:11:42 -0700

    update

--- app.js
+++ app.js
@@ -41,6 +41,17 @@ app.post('/president',(req,res)=>{
     
 })
 
+app.get('/president/:id',(req,res)=>{
+    const id=req.params.id
+    console.log(id);
+    
+   const presi= presidents.find(c=> c.id===parseInt(req.params.id))
+    
+
+    if (!presidents) res.status(404).send('president not found');
+    res.render('details',{presidents:presi, title:"The president"});
+})
+
 app.get('/president/create', (req ,res) =>{
     res.render('create',{title:"New President"});
 })
